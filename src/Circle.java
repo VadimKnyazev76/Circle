15f1782afe495ff95f9480ae9b7bec302a674ea0
VadimKnyazev76 <vadim.knyazev@gmail.com>
2021-01-24 22:57:28 +0200

    Circle

---
+++ src/Circle.java
@@ -0,0 +1,8 @@
+public class Circle {
+    public static void main(String[] args) {
+        double PI = 3.1415926535897932384626D;
+        int r = 4;
+        double S = PI*Math.pow(r,2);
+        System.out.println(S);
+    }
+}
